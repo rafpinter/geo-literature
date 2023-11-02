b813400979199f16328e25d8b18c6b37b7cc9892
Rafaela Pinter <rpinter811@gmail.com>
2023-11-02 13:49:09 -0400

    loading dotenv

--- configs/vars.py
+++ configs/vars.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # take environment variables from .env.
-# load_dotenv()
+load_dotenv()
 
 APP_TITLE = os.getenv("APP_TITLE", "Geo-Lit")
 APP_FAVICON = os.getenv("APP_FAVICON", "favicon.png")
